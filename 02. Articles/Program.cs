eb3b906c7e39b7f35327b8771a34262c78f492d9
Tanya Yordanova <110428464+tanya-dim-yo@users.noreply.github.com>
2024-01-14 01:33:42 +0200

    Add files via upload

---
+++ 02. Articles/Program.cs
@@ -0,0 +1,79 @@
+﻿namespace _02.Articles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] articleParts = Console.ReadLine().
+                Split(", ", StringSplitOptions.RemoveEmptyEntries).
+                ToArray();
+
+            string title = articleParts[0];
+            string content = articleParts[1];   
+            string author = articleParts[^1];
+
+            Article article = new Article(title, content, author);
+
+            int numberOfCommands = int.Parse(Console.ReadLine());
+
+            for (int i = 0;  i < numberOfCommands; i++)
+            {
+                string[] commandTokens = Console.ReadLine()
+                    .Split(": ", StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+
+                string command = commandTokens[0];
+                string newValue = commandTokens[1];
+
+                if (command == "Edit")
+                {
+                    article.Edit(newValue);
+                }
+                else if (command == "ChangeAuthor")
+                {
+                    article.ChangeAuthor(newValue);
+                }
+                else
+                {
+                    article.Rename(newValue);
+                }
+            }
+
+            Console.WriteLine(article);
+        }
+    }
+
+    public class Article
+    {
+        public Article(string title, string content, string author)
+        {
+            Title = title;
+            Content = content;
+            Author = author;
+        }
+
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string Author { get; set; }
+
+        public void Edit(string content)
+        {
+            Content = content;
+        }
+
+        public void ChangeAuthor(string author)
+        {
+            Author = author;
+        }
+
+        public void Rename(string title)
+        {
+            Title = title;
+        }
+
+        public override string ToString()
+        {
+            return $"{Title} - {Content}: {Author}";
+        }
+    }
+}
