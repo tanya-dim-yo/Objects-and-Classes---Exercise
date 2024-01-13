e4e133430fa6b5fa7978f5941b21e76756edd1c1
Tanya Yordanova <110428464+tanya-dim-yo@users.noreply.github.com>
2024-01-14 01:34:21 +0200

    Add files via upload

---
+++ 03. Articles 2.0/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace _02.Articles_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfArticles = int.Parse(Console.ReadLine());
+
+            List<Article> articles = new List<Article>();
+
+            for (int i = 0; i < numberOfArticles; i++)
+            {
+                string[] articleParts = Console.ReadLine().
+                    Split(", ", StringSplitOptions.RemoveEmptyEntries).
+                    ToArray();
+
+                string title = articleParts[0];
+                string content = articleParts[1];
+                string author = articleParts[^1];
+
+                Article article = new Article(title, content, author);
+                articles.Add(article);
+            }
+
+            foreach (var article in articles)
+            {
+                Console.WriteLine(article);
+            }
+        }
+    }
+
+    public class Article
+    {
+        public Article(string title, string content, string author)
+        {
+            Title = title;
+            Content = content;
+            Author = author;
+        }
+
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string Author { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Title} - {Content}: {Author}";
+        }
+    }
+}
