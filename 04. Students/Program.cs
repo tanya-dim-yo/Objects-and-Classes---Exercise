fe9d29ca793685041d751cf2a0f22b351d34a5dd
Tanya Yordanova <110428464+tanya-dim-yo@users.noreply.github.com>
2024-01-15 20:45:44 +0200

    Add files via upload

---
+++ 04. Students/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace _04.Students
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int countOfStudents = int.Parse(Console.ReadLine());
+
+            List<Student> students = new List<Student>();
+
+            for (int i = 0;  i < countOfStudents; i++)
+            {
+                string[] currentStudent = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+
+                string firstName = currentStudent[0];
+                string lastName = currentStudent[1];
+                double grade = double.Parse(currentStudent[2]);
+
+                Student student = new Student(firstName, lastName, grade);
+                students.Add(student);
+            }
+
+            foreach (var student in students.OrderByDescending(x => x.Grade))
+            {
+                Console.WriteLine(student);
+            }
+        }
+    }
+
+    public class Student
+    {
+        public Student(string firstName, string lastName, double grade)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Grade = grade;
+        }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public double Grade { get; set; }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName}: {Grade:F2}";
+        }
+    }
+}
